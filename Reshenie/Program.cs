89b41f33ce77a8d8e369553df99c2e392d1d85ca
KonotopOlga <ol.konotop@yandex.ru>
2022-08-17 19:21:15 +0300

    reshenie naideno

---
+++ Reshenie/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+/* Введем цикл, в котором через будет оставаться остаток от деления на 10. 
+Это же самое число делим на 10 такое количество раз, чтобы получился ноль.*/
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int number = userNumber;
+
+while(number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+    Console.WriteLine(sum);
+}
+Console.WriteLine($"Сумма всех цифр числа {userNumber} равна {sum}");
